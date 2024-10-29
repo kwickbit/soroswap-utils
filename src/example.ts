c6acd8f44dac429fb375f1725304da1810b34ec2
Bruno Parga <brunoparga@gmail.com>
2024-10-29 18:29:57 +0200

    Rename a file for dev convenience

---
+++ src/example.ts
@@ -0,0 +1,17 @@
+import { Mercury } from "mercury-sdk";
+import "dotenv/config";
+
+(async () => {
+    const mercury = new Mercury({
+        backendEndpoint: process.env.MERCURY_BACKEND_ENDPOINT!,
+        graphqlEndpoint: process.env.MERCURY_GRAPHQL_ENDPOINT!,
+        email: process.env.MERCURY_TESTER_EMAIL!,
+        password: process.env.MERCURY_TESTER_PASSWORD!,
+    });
+
+    const response = await mercury?.subscribeToContractEvents({
+        contractId: process.env.SOROSWAP_FACTORY_CONTRACT!,
+    });
+
+    console.log(response);
+})();
