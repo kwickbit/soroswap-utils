6aac1030f253231962b5b7875b2f84144de15678
Bruno Parga <brunoparga@gmail.com>
2024-11-05 18:47:21 +0200

    Configure eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,21 +1,21 @@
+"use strict";
+
 module.exports = {
-    root: true,
-    env: {
-        node: true,
-        es2022: true
-    },
     extends: [
         "hardcore",
         "hardcore/ts",
         "hardcore/jest",
         "prettier"
     ],
-    parser: "@typescript-eslint/parser",
+
     parserOptions: {
-        project: "./tsconfig.json",
-        ecmaVersion: 2022
+        project: "./tsconfig.eslint.json"
     },
-    plugins: [
-        "@typescript-eslint"
-    ]
+
+    root: true,
+
+    rules: {
+        "putout/putout": "off",
+        "unicorn/import-index": "off",
+    }
 }
