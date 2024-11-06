ef8b3d594129200feef56fc2d8ca7eda7b2810f6
Bruno Parga <brunoparga@gmail.com>
2024-11-06 14:35:53 +0200

    Set up desired ESLint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,21 +1,38 @@
 "use strict";
 
+const maximumLineLength = 100;
+
 module.exports = {
-    extends: [
-        "hardcore",
-        "hardcore/ts",
-        "hardcore/jest",
-        "prettier"
-    ],
+    extends: ["hardcore", "hardcore/ts", "hardcore/jest", "prettier"],
 
     parserOptions: {
-        project: "./tsconfig.eslint.json"
+        project: "./tsconfig.eslint.json",
     },
+
+    plugins: ["prettier"],
 
     root: true,
 
     rules: {
+        "@stylistic/max-len": [
+            "error",
+            {
+                code: maximumLineLength,
+                ignorePattern: String.raw`^\s*//\s*eslint-disable`,
+            },
+        ],
+
+        "@typescript-eslint/consistent-type-assertions": [
+            "error",
+            { assertionStyle: "as", objectLiteralTypeAssertions: "allow" },
+        ],
+
+        "@typescript-eslint/no-throw-literal": "off",
+        "compat/compat": "off",
+        "func-style": ["error", "declaration", { allowArrowFunctions: true }],
+        "no-console": "off",
+        "prettier/prettier": ["error", { printWidth: maximumLineLength, tabWidth: 4 }],
         "putout/putout": "off",
         "unicorn/import-index": "off",
-    }
-}
+    },
+};
