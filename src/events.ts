0a1461c1102ea23fcee60d7f910339bb9f2e7b4a
Bruno Parga <brunoparga@gmail.com>
2024-11-14 12:48:55 +0200

    Subscribe to SoroswapPair contract events

--- src/events.ts
+++ src/events.ts
@@ -3,11 +3,14 @@ import { getContractEventsParser } from "mercury-sdk";
 
 import { buildMercuryInstance, getEnvironmentVariable } from "./utils";
 
-const getSoroswapEvents = async (contract: string): Promise<unknown> => {
+const fetchSoroswapEvents = async (
+    contractId: string,
+    isEnvironmentVariable = false,
+): Promise<unknown> => {
     const mercuryInstance = buildMercuryInstance();
 
     const soroswapEvents = await mercuryInstance.getContractEvents({
-        contractId: getEnvironmentVariable(contract),
+        contractId: isEnvironmentVariable ? getEnvironmentVariable(contractId) : contractId,
     });
 
     if (soroswapEvents.error !== undefined) {
@@ -27,7 +30,7 @@ const getSoroswapEvents = async (contract: string): Promise<unknown> => {
  * @throws {Error} If the events cannot be read.
  */
 export const getSoroswapFactoryEvents = async (): Promise<unknown> =>
-    await getSoroswapEvents("SOROSWAP_FACTORY_CONTRACT");
+    await fetchSoroswapEvents("SOROSWAP_FACTORY_CONTRACT", true);
 
 /**
  * Retrieve Soroswap Router contract events.
@@ -35,4 +38,13 @@ export const getSoroswapFactoryEvents = async (): Promise<unknown> =>
  * @throws {Error} If the events cannot be read.
  */
 export const getSoroswapRouterEvents = async (): Promise<unknown> =>
-    await getSoroswapEvents("SOROSWAP_ROUTER_CONTRACT");
+    await fetchSoroswapEvents("SOROSWAP_ROUTER_CONTRACT");
+
+/**
+ * Retrieve events from a given Soroswap Pair contract.
+ * @param contractId The contract ID of the Soroswap Pair contract.
+ * @returns {Promise<unknown>} A promise that resolves to the event array.
+ * @throws {Error} If the events cannot be read.
+ */
+export const getSoroswapPairEvents = async (contractId: string): Promise<unknown> =>
+    await fetchSoroswapEvents(contractId);
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 // Rule disabled because this is a library, to be imported by other packages
 /* eslint-disable import/no-unused-modules */
 export { getAssetData, isCertifiedAsset, listCertifiedAssets } from "./assets";
-export { getSoroswapFactoryEvents, getSoroswapRouterEvents } from "./events";
+export { getSoroswapFactoryEvents, getSoroswapPairEvents, getSoroswapRouterEvents } from "./events";
 export { getLiquidityPoolAddresses, getLiquidityPoolCount, getLiquidityPoolData } from "./pools";
 export {
     subscribeToSoroswapFactory,
